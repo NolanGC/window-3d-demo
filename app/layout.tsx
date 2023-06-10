12258b841d9ae18d70b9805c45fe9feba6f8c13d
Nolan Clement <nolanclement@Nolans-MacBook-Pro.local>
2023-06-10 23:44:38 +0800

    clean

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,39 @@
 import "@/styles/globals.css"
 import { Metadata } from "next"
+
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
-import { Toaster } from "@/components/ui/toaster"
-import Head  from "next/head"
 
-// export const metadata: Metadata = {
-//   openGraph: {
-//     title: "test",
-//     description: "test",
-//   },
-//   title: {
-//     default: "test",
-//     template: `%s - ${"test"}`,
-//   },
-//   description: "test",
-//   themeColor: [
-//     { media: "(prefers-color-scheme: light)", color: "white" },
-//     { media: "(prefers-color-scheme: dark)", color: "black" },
-//   ],
-//   icons: {
-//     icon: "/favicon.ico",
-//     shortcut: "/favicon-16x16.png",
-//     apple: "/apple-touch-icon.png",
-//   },
-// }
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+}
 
+interface RootLayoutProps {
+  children: React.ReactNode
+}
 
-export default function Layout({ children }) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
+    <>
       <html lang="en" suppressHydrationWarning>
+        <head />
         <body
           className={cn(
             "min-h-screen bg-background font-sans antialiased",
@@ -47,9 +47,8 @@ export default function Layout({ children }) {
             </div>
             <TailwindIndicator />
           </ThemeProvider>
-          <Toaster />
         </body>
-        
       </html>
+    </>
   )
 }
