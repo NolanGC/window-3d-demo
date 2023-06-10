f72f181d56d2637ef7a503b901bab574ff508432
Nolan Clement <nolanclement@Nolans-MacBook-Pro.local>
2023-06-10 18:03:04 +0800

    metadatabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,9 +40,6 @@ export const metadata = {
 export default function Layout({ children }) {
   return (
       <html lang="en" suppressHydrationWarning>
-        <Head>
-          <title>Window</title>
-        </Head>
         <body
           className={cn(
             "min-h-screen bg-background font-sans antialiased",
