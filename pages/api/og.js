db689d7f4152dbd00338b55456fee80fd754bf86
Nolan Clement <nolanclement@Nolans-MacBook-Pro.local>
2023-06-11 22:14:24 +0800

    dynamic route

--- pages/api/og.js
+++ pages/api/og.js
@@ -5,73 +5,43 @@ export const config = {
   runtime: 'edge',
 };
  
-export default function handler(request) {
+export default async function handler(request) {
   try {
     const { searchParams } = new URL(request.url);
  
     // ?title=<title>
-    const hasTitle = searchParams.has('id');
-    const title = hasTitle
+    const hasId = searchParams.has('id');
+    const id = hasId
       ? searchParams.get('id')
       : "no id provided"
- 
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            backgroundColor: 'black',
-            backgroundSize: '150px 150px',
-            height: '100%',
-            width: '100%',
-            display: 'flex',
-            textAlign: 'center',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            flexWrap: 'nowrap',
-          }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              justifyItems: 'center',
-            }}
-          >
-            <img
-              alt="Vercel"
-              height={200}
-              src="data:image/svg+xml,%3Csvg width='116' height='100' fill='white' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M57.5 0L115 100H0L57.5 0z' /%3E%3C/svg%3E"
-              style={{ margin: '0 30px' }}
-              width={232}
-            />
-          </div>
-          <div
-            style={{
-              fontSize: 60,
-              fontStyle: 'normal',
-              letterSpacing: '-0.025em',
-              color: 'white',
-              marginTop: 30,
-              padding: '0 120px',
-              lineHeight: 1.4,
-              whiteSpace: 'pre-wrap',
-            }}
-          >
-            {title}
-          </div>
+    if (!hasId) {
+      return ImageResponse(
+        <div>
+          <image src="https://i.imgur.com/5O3UZtA.png"/>
         </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-      },
-    );
-  } catch (e) {
-    console.log(`${e.message}`);
-    return new Response(`Failed to generate the image`, {
-      status: 500,
-    });
+      )
+    }
+    else {
+      const databaseItem = await fetch(`/api/find?id=${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      const databaseItemJson = await databaseItem.json();
+      const imageThumbnail = databaseItemJson[0].image_thumbnail;
+      return ImageResponse(
+        <div>
+          <image src={imageThumbnail}/>
+        </div>
+      )
+    }
+  } catch (error) {
+    console.error(error);
+    return ImageResponse(
+      <div>
+        <image src="https://i.imgur.com/5O3UZtA.png"/>
+      </div>
+    )
   }
-}
+} 
