165a87d9ed9b8d8defc1c958e9f7f5ea7161edfc
Nolan Clement <nolanclement@Nolans-MacBook-Pro.local>
2023-06-12 10:41:54 +0800

    nice fallback

--- pages/api/og.js
+++ pages/api/og.js
@@ -6,6 +6,56 @@ export const config = {
   runtime: 'edge',
 };
  
+const fallback = new ImageResponse(
+  <div
+  style={{
+    display: 'flex',
+    height: '100%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundImage: 'linear-gradient(to bottom, #dbf4ff, #fff1f1)',
+    fontSize: 60,
+    letterSpacing: -2,
+    fontWeight: 700,
+    textAlign: 'center',
+  }}
+  >
+  <div
+    style={{
+      backgroundImage: 'linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))',
+      backgroundClip: 'text',
+      '-webkit-background-clip': 'text',
+      color: 'transparent',
+    }}
+  >
+    Generate
+  </div>
+  <div
+    style={{
+      backgroundImage: 'linear-gradient(90deg, rgb(121, 40, 202), rgb(255, 0, 128))',
+      backgroundClip: 'text',
+      '-webkit-background-clip': 'text',
+      color: 'transparent',
+    }}
+  >
+    3D
+  </div>
+  <div
+    style={{
+      backgroundImage: 'linear-gradient(90deg, rgb(255, 77, 77), rgb(249, 203, 40))',
+      backgroundClip: 'text',
+      '-webkit-background-clip': 'text',
+      color: 'transparent',
+    }}
+  >
+    Objects 
+  </div>
+</div>
+
+)
+
 export default async function handler(request) {
   try {
     const { searchParams } = new URL(request.url);
@@ -15,32 +65,26 @@ export default async function handler(request) {
       : "no id provided"
     
     if (!hasId) {
-      return new ImageResponse(<img src="https://i.imgur.com/DMcWUu0.png"/>, {
-        width: 1200,
-        height: 630,
-      });
+      return fallback;
     }
     else {
-      // const databaseItem = await fetch(`https://window-3d-demo.vercel.app/api/find?id=${id}`, {
-      //   method: "GET",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      // })
-      //console.log("database item")
-      // const databaseItemJson = await databaseItem.json();
-      //console.log(Object.keys(databaseItemJson[0]))
-      // const imageThumbnail = databaseItemJson[0].thumbnail_uri;
-      return new ImageResponse(<img src="https://i.imgur.com/DMcWUu0.png"/>, {
+      const databaseItem = await fetch(`https://window-3d-demo.vercel.app/api/find?id=${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      
+      const databaseItemJson = await databaseItem.json();
+      console.log(Object.keys(databaseItemJson[0]))
+      const imageThumbnail = databaseItemJson[0].thumbnail_uri;
+      return new ImageResponse(<img src={imageThumbnail}/>, {
         width: 1200,
         height: 630,
       });
     }
   } catch (error) {
    console.error(error);
-   return new ImageResponse(<img src="https://i.imgur.com/DMcWUu0.png"/>, {
-     width: 1200,
-     height: 630,
-   });
+   return fallback;
  }
 } 
