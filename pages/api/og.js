67cc1f077330bb860824f07ccb37eb55d63ca6b5
Nolan Clement <nolanclement@Nolans-MacBook-Pro.local>
2023-06-12 10:19:37 +0800

    back to template

--- pages/api/og.js
+++ pages/api/og.js
@@ -1,5 +1,50 @@
+// import { ImageResponse } from '@vercel/og';
+// import { data } from 'autoprefixer';
+// import { NextRequest } from 'next/server';
+ 
+// export const config = {
+//   runtime: 'edge',
+// };
+ 
+// export default async function handler(request) {
+//   try {
+//     const { searchParams } = new URL(request.url);
+//     const hasId = searchParams.has('id');
+//     const id = hasId
+//       ? searchParams.get('id')
+//       : "no id provided"
+    
+//     if (!hasId) {
+//       return new ImageResponse(<img src="https://i.imgur.com/DMcWUu0.png"/>, {
+//         width: 1200,
+//         height: 630,
+//       });
+//     }
+//     else {
+//       // const databaseItem = await fetch(`https://window-3d-demo.vercel.app/api/find?id=${id}`, {
+//       //   method: "GET",
+//       //   headers: {
+//       //     "Content-Type": "application/json",
+//       //   },
+//       // })
+//       //console.log("database item")
+//       // const databaseItemJson = await databaseItem.json();
+//       //console.log(Object.keys(databaseItemJson[0]))
+//       // const imageThumbnail = databaseItemJson[0].thumbnail_uri;
+//       return new ImageResponse(<img src="https://i.imgur.com/DMcWUu0.png"/>, {
+//         width: 1200,
+//         height: 630,
+//       });
+//     }
+//   } catch (error) {
+//    console.error(error);
+//    return new ImageResponse(<img src="https://i.imgur.com/DMcWUu0.png"/>, {
+//      width: 1200,
+//      height: 630,
+//    });
+//  }
+// } 
 import { ImageResponse } from '@vercel/og';
-import { data } from 'autoprefixer';
 import { NextRequest } from 'next/server';
  
 export const config = {
@@ -7,40 +52,44 @@ export const config = {
 };
  
 export default async function handler(request) {
-  try {
-    const { searchParams } = new URL(request.url);
-    const hasId = searchParams.has('id');
-    const id = hasId
-      ? searchParams.get('id')
-      : "no id provided"
-    
-    if (!hasId) {
-      return new ImageResponse(<img src="https://i.imgur.com/DMcWUu0.png"/>, {
-        width: 1200,
-        height: 630,
-      });
-    }
-    else {
-      // const databaseItem = await fetch(`https://window-3d-demo.vercel.app/api/find?id=${id}`, {
-      //   method: "GET",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      // })
-      //console.log("database item")
-      // const databaseItemJson = await databaseItem.json();
-      //console.log(Object.keys(databaseItemJson[0]))
-      // const imageThumbnail = databaseItemJson[0].thumbnail_uri;
-      return new ImageResponse(<img src="https://i.imgur.com/DMcWUu0.png"/>, {
-        width: 1200,
-        height: 630,
-      });
-    }
-  } catch (error) {
-   console.error(error);
-   return new ImageResponse(<img src="https://i.imgur.com/DMcWUu0.png"/>, {
-     width: 1200,
-     height: 630,
-   });
- }
-} 
+  const { searchParams } = request.nextUrl;
+  const username = searchParams.get('id');
+  if (!username) {
+    return new ImageResponse(<>Visit with &quot;?username=vercel&quot;</>, {
+      width: 1200,
+      height: 630,
+    });
+  }
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          fontSize: 60,
+          color: 'black',
+          background: '#f6f6f6',
+          width: '100%',
+          height: '100%',
+          paddingTop: 50,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <img
+          width="256"
+          height="256"
+          src={`https://github.com/${username}.png`}
+          style={{
+            borderRadius: 128,
+          }}
+        />
+        <p>github.com/{username}</p>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
+}
