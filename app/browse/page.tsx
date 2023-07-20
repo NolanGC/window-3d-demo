afd939a4ac0772352917ae0a7645e7596994c537
Nolan Clement <nolangclement@gmail.com>
2023-07-20 14:33:15 -0700

    fix browse

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -44,7 +44,7 @@ import { useToast } from "@/components/ui/use-toast";
 
 
     const handleInspectClick = (id: any) => {
-      router.push(`/?id=${id}`);
+      router.push(`https://openrouter-3d-generator.vercel.app/?id=${id}`);
     };
 
     useEffect(() => {
